a84bde767c727927a81f9301fb7a4878ac0483e3
Akshita Sharma <113848272+shibbolithic@users.noreply.github.com>
2024-05-06 13:11:36 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -44,7 +44,7 @@ class AdminPage:
         self.db_connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="9871618879",
+            password="*****",
             database="amazon"
         )
         self.cursor = self.db_connection.cursor()
