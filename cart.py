4a02f4e73426913d7834f1f4c96004c272d35f43
Akshita Sharma <113848272+shibbolithic@users.noreply.github.com>
2024-05-06 13:13:38 +0530

    Update cart.py

--- cart.py
+++ cart.py
@@ -53,7 +53,7 @@ class CartPage:
         self.db_connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="9871618879",
+            password="****",
             database="amazon"
         )
         self.cursor = self.db_connection.cursor()
