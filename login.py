effab2de3634b7210569e6f595f4612ecaa3e563
Akshita Sharma <113848272+shibbolithic@users.noreply.github.com>
2024-06-02 08:04:19 +0530

    Update login.py

--- login.py
+++ login.py
@@ -45,7 +45,7 @@ class LoginForm:
         self.db_connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="9871618879",
+            password="####",
             database="amazon"
         )
         self.cursor = self.db_connection.cursor()
